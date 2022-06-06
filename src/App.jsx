4e5fc4c463386f2aae0eacb490b2dbac35525c5f
erikaperciliano <erikaperciliano.developer@gmail.com>
2022-06-06 20:04:15 -0300

    adds components at App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
+
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
-    <Header>
-      <h1>
-        Hello World
-      </h1>
-    </Header>
+    <>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Erika Perciliano"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          />
+          <Post
+            author="Marcelo Sanches"
+            content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+          />
+        </main>
+      </div>
+    </>
   )
 }
 
