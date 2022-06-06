f3e5f95ea9dfa0b245011ada377c87de234c523a
erikaperciliano <erikaperciliano.developer@gmail.com>
2022-06-06 18:51:17 -0300

    add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Header } from './components/Header';
 import './styles.css';
 
 function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <Header>
+      <h1>
+        Hello World
+      </h1>
+    </Header>
   )
 }
 
