eba590757471efa4647aa150142f894719397757
erikaperciliano <erikaperciliano.developer@gmail.com>
2022-06-15 12:34:34 -0300

    add styles of Comment Page

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -15,7 +15,7 @@ export function Comment() {
                         </div>
 
                         <button title='Remove comment'>
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
 
