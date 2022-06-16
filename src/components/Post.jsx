50c6a5dc1d1d6e95997e65cccc3fb8605d6b5d7d
erikaperciliano <erikaperciliano.developer@gmail.com>
2022-06-16 20:15:51 -0300

    creates the function of delete comment and pass this as property to Comment Page

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { formatDistanceToNow } from 'date-fns';
 import enUS from 'date-fns/locale/en-US';
+import { Key } from 'phosphor-react';
 import { useState } from 'react';
 
 import { Avatar } from './Avatar';
@@ -31,6 +32,10 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(comment) {
+        console.log(`Delete Comment:${comment}`)
+    }
+
     return(
         <article className={styles.post}>
             <header>
@@ -50,9 +55,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                {content.map(line => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     }else if(line.type === 'link') {
-                        return <p> 👉 {' '}<a href="">{line.content}</a></p>
+                        return <p key={line.content}> 👉 {' '}<a href="">{line.content}</a></p>
                     }
                })}
             </div>
@@ -74,7 +79,13 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                {comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return (
+                        <Comment 
+                            key={comment} 
+                            content={comment} 
+                            onDeleteComment={deleteComment}
+                        />
+                    )
                })}
             </div>
         </article>
