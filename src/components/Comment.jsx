28a8cae2702e614a78da3ed95c811ec34e53b225
erikaperciliano <erikaperciliano.developer@gmail.com>
2022-06-16 21:06:11 -0300

    add functionality of clap

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return(
@@ -28,9 +35,9 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                     <footer>
-                        <button>
+                        <button onClick={handleLikeComment}>
                             <ThumbsUp />
-                            Clap <span>20</span>
+                            Clap <span>{likeCount}</span>
                         </button>
                     </footer>
             </div>
